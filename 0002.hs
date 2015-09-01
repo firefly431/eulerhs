3b100d12d6d2c322140273f209b67c3b0875ecdd
firefly431 <firefly431.awesome@gmail.com>
2015-09-01 14:14:51 -0500

    Use even function

--- 0002.hs
+++ 0002.hs
@@ -1,4 +1,3 @@
 import qualified Fibonacci as F
-import qualified Number as N
 
-main = print . sum $ filter (N.divisibleBy 2) $ takeWhile (<4000000) F.fibs
+main = print . sum $ filter even $ takeWhile (<4000000) F.fibs
