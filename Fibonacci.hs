31270134a3a26ed92ab1a14510df6f8fecf8cd8c
firefly431 <firefly431.awesome@gmail.com>
2015-09-01 14:01:54 -0500

    Fix syntax error and document

--- Fibonacci.hs
+++ Fibonacci.hs
@@ -4,13 +4,16 @@ module Fibonacci
 , fib
 ) where
 
-fibc :: (Num a) -> a -> a -> [a]
+-- |The 'fibc' function produces a Fibonacci-characteristic sequences given the first two values.
+fibc :: (Num a) => a -> a -> [a]
 fibc a b = seq
   where seq = a : b : zipWith (+) seq (tail seq)
 
+-- |The 'fibs' function produces the Fibonacci sequence.
 fibs :: (Num a) => [a]
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibs = fibc 0 1
 
 -- TODO: optimize
+-- |The 'fib' function returns the nth Fibonacci number.
 fib :: (Num a) => Int -> a
 fib n = fibs !! n
