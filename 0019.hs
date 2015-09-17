caebaa0c5dc69f43b75b4b389555f6f8d8f98fba
James <dong.james.sept14@gmail.com>
2015-09-17 13:56:18 -0500

    Solve 19

--- 0019.hs
+++ 0019.hs
@@ -2,4 +2,8 @@ notleap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 leap    = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 -- starting from 1901
-monthdays = concat $ (replicate 3 notleap ++ (leap : replicate 3 notleap))
+monthdays = concat . cycle $ [notleap, notleap, notleap, leap]
+
+-- Jan 1, 1901 was a Tuesday
+
+main = print . length . filter (==0) . scanl (\a b -> (a + b) `mod` 7) 2 . take 1200 $ monthdays
